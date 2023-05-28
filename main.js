0930f5b229eca1a0de8f5f4bd5250e38f75a0d18
manishatamang752@gmail.com <manishatamang752@gmail.com>
2023-05-28 12:03:01 +0545

    project

--- main.js
+++
@@ -1,9 +0,0 @@
-let search =document.querySelector('.search-box');
-document.querySelector('#search-icon').oneclick =() =>{
-    search.classList.toggle('active'); 
-}
-let header =document.querySelector('header');
-window.addEventListener('scroll' ,() => {
-
-    header.classList.toggle('shadow', window.scrollY> 0)
-})
